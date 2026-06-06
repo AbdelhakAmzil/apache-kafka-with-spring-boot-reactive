e40a968568009c1def8a51679f45c9f46a1320d5
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-06-06 12:34:35 +0100

    create student payload object

--- kafka-demo/src/main/java/com/abdel/kafkademo/consumer/KafkaConsumer.java
+++ kafka-demo/src/main/java/com/abdel/kafkademo/consumer/KafkaConsumer.java
@@ -1,6 +1,6 @@
 package com.abdel.kafkademo.consumer;
 
-import com.abdel.kafka.payload.Student;
+import com.abdel.kafkademo.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
