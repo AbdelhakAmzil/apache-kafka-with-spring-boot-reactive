cf283b532a066164a4b0800690ecff3a8781350a
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-06-06 12:38:31 +0100

    add kafka producers

---
+++ kafka-demo/src/main/java/com/abdel/kafkademo/producer/KafkaJsonProducer.java
@@ -0,0 +1,26 @@
+package com.abdel.kafkademo.producer;
+
+import com.abdel.kafkademo.payload.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaJsonProducer {
+
+    private final KafkaTemplate<String, Student> kafkaTemplate;
+
+    public void sendMessage(Student student) {
+
+        Message<Student> message = MessageBuilder
+                .withPayload(student)
+                .setHeader(KafkaHeaders.TOPIC, "abdel")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
