4d2628730b7f1a5e27d7c785f3268bb99041fb49
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-06-06 12:18:32 +0100

    add docker-compose and kafka-demo project

---
+++ kafka-demo/src/test/java/com/abdel/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdel.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
